aa82232af7f1eb2ef5551f90897ad9672f118e31
Mr.User <test@test.test>
2018-04-14 11:46:17 +0800

    opt user/index.vue add icon

--- src/view/user/index.vue
+++ src/view/user/index.vue
@@ -11,34 +11,57 @@
         <card>
           <div slot="content" class="card-flex">
             <router-link to="/user/profile" class="card-item vux-1px-r">
-              <span>1130</span>
+              <div>
+                <i class="iconfont icon-roundadd"></i>
+              </div>
               <div>未读通知</div>
+              <span>1130</span>
             </router-link>
             <router-link to="/user/profile" class="card-item vux-1px-r">
-              <span>15</span>
+              <div>
+                <i class="iconfont icon-roundadd"></i>
+              </div>
               <div>个人积分</div>
+              <span>15</span>
             </router-link>
             <router-link to="/user/profile" class="card-item">
-              <span>0</span>
+              <div>
+                <i class="iconfont icon-roundadd"></i>
+              </div>
               <div>党建动态</div>
+              <span>0</span>
             </router-link>
           </div>
         </card>
         <group>
           <cell title="个人资料" link="/user/profile" value="白求恩">
-            <img slot="icon" style="display:block;margin-right:5px;" src="http://dn-placeholder.qbox.me/110x110/FF2D55/000" width="24" height="24">
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
           </cell>
-          <cell title="微信绑定" @click.native="wechat" is-link value="某某人"></cell>
-          <cell title="修改密码" link="/user/password" value="当前密码已使用865天"></cell>
+          <cell title="微信绑定" @click.native="wechat" is-link value="某某人">
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
+          <cell title="修改密码" link="/user/password" value="当前密码已使用865天">
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
         </group>
         <group>
-          <cell title="个人积分" @click.native="log" is-link value="0"></cell>
-          <cell title="党费查询" @click.native="log" is-link></cell>
+          <cell title="个人积分" @click.native="log" is-link value="0">
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
+          <cell title="党费查询" @click.native="log" is-link>
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
         </group>
         <group>
-          <cell title="通知公告" @click.native="log" is-link></cell>
-          <cell title="党务公开" @click.native="log" is-link></cell>
-          <cell title="党建动态" @click.native="log" is-link></cell>
+          <cell title="通知公告" @click.native="log" is-link>
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
+          <cell title="党务公开" @click.native="log" is-link>
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
+          <cell title="党建动态" @click.native="log" is-link>
+            <i slot="icon" class="listIcon iconfont icon-roundadd"></i>
+          </cell>
         </group>
         <div v-transfer-dom>
           <confirm v-model="wechatDialog" :title="'解除绑定'" @on-confirm="wechatDialogConfirm" @on-cancel="onCancel" @on-show="onShow" @on-hide="onHide">
@@ -134,8 +157,14 @@ export default {
   font-size 12px
   padding 20px 0
 }
-.card-flex span {
-  color #f74c31
+.card-item {
+  i {
+    font-size 36px
+    color #EEE
+  }
+  span {
+    color #f74c31
+  }
 }
 .vux-1px-r {
   position relative
@@ -153,5 +182,12 @@ export default {
   transform-origin 100% 0
   -webkit-transform scaleX(0.5)
   transform scaleX(0.5)
+}
+.listIcon {
+  font-size 24px
+  color #CCC
+  line-height 1
+  float left
+  margin-right 5px
 }
 </style>
