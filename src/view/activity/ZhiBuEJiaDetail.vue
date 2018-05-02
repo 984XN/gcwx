c6c245625acba76118e541838eefe8f11179a079
Mr.User <test@test.test>
2018-05-02 14:46:50 +0800

    add 支部E家详情页接口绑定中（测试数据需要使用 新增接口，故暂停）

--- src/view/activity/ZhiBuEJiaDetail.vue
+++ src/view/activity/ZhiBuEJiaDetail.vue
@@ -1,3 +1,77 @@
 <template>
-  <div>ActivityZhiBuEJiaDetail</div>
+  <Article :article="article" @online="online"></Article>
 </template>
+
+<script>
+import Article from 'src/components/article';
+import * as api from 'src/api/activity';
+
+export default {
+  components: {
+    Article
+  },
+  data() {
+    return {
+      status: {
+        viewed: false // 是否已经标记成了已读
+      },
+      setViewedThreshold: 30, // 这个秒数后访问量+1
+      viewSecond: 0, // 已阅读秒数
+      readyTime: null, // 页面加载完成时的时间戳
+      article: { baseInfo: {}, files: [] }
+    };
+  },
+  methods: {
+    setViewd() {
+      let self = this;
+      api.activity.setViewd({ ID: self.article.baseInfo.id }).then(res => {
+        // console.log('setViewd by online:', res);
+      });
+    },
+    // 文档停留30秒才算阅读过
+    online(second) {
+      let self = this;
+      // let type = self.article.baseInfo.type;
+      self.viewSecond = second;
+      // console.log('online:', second, 'sec. type:' + type);
+      // 30秒即可标记为已读
+      if (!self.status.viewed && self.viewSecond > self.setViewedThreshold) {
+        self.status.viewed = true; // api 200 ok 时再标记会导致请求过程中多次执行标记操作
+        api.activity.setViewd({ ID: self.article.baseInfo.id }).then(res => {
+          // console.log('setViewd by online:', res);
+        });
+      }
+    }
+  },
+  mounted() {
+    // 页面加载完成时的时间戳
+    this.readyTime = this.time();
+    this.$nextTick(function() {
+      // console.log('this.$route', this.$route);
+      let id = this.$route.params.id || 0;
+      if (!id) {
+        this.$router.replace({
+          path: '/',
+          query: { error: 'missing-id', from: this.$route.fullPath }
+        });
+      }
+      this.$vux.loading.show({
+        text: '正在获取数据'
+      });
+      api.activity
+        .getOne({
+          ID: id
+        })
+        .then(res => {
+          this.$vux.loading.hide();
+          this.article = res.Data.Article;
+          // console.log('getOne:', res);
+        })
+        .catch(() => {
+          this.$vux.loading.hide();
+          this.$router.go(-1);
+        });
+    });
+  }
+};
+</script>
