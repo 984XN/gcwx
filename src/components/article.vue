2b77938fb5e090960663530f195baf39e483fb7e
Mr.User <test@test.test>
2018-05-03 15:37:55 +0800

    opt article.vue 组件重构

--- src/components/article.vue
+++ src/components/article.vue
@@ -10,7 +10,13 @@
     </div>
     <div class="article-body">
       <div v-html="article.baseInfo.content" v-if="article.baseInfo.content" class="article-body-content"></div>
-      <div v-if="article.baseInfo.type === 10" class="article-body-files article-body-files-video">
+      <div v-if="article.imgs && article.imgs.length" class="article-body-files article-body-files-img">
+        <div class="weui-cells__title">图片列表</div>
+        <div class="article-body-files-img-body">
+          <div v-for="(img, index) in article.imgs" :key="index"><img :src="img.src" alt="加载中"></div>
+        </div>
+      </div>
+      <div v-if="article.videos && article.videos.length" class="article-body-files article-body-files-video">
         <div class="weui-cells__title">视频列表</div>
         <div class="article-body-files-video-body">
           <div class="file-item" v-for="(val, index) in article.files" :key="index">
@@ -20,7 +26,7 @@
           </div>
         </div>
       </div>
-      <group :title="'文件清单 (共' + article.files.length + '个)'" v-if="article.files.length && article.baseInfo.type === 20" class="article-body-files article-body-files-document">
+      <group :title="'文件清单 (共' + article.files.length + '个)'" v-if="article.files && article.files.length" class="article-body-files article-body-files-document">
         <cell :title="(index+1) + '、' + val.name" :link="getArticleLink(val.path)" v-for="(val, index) in article.files" :key="index" class="file-item"></cell>
       </group>
     </div>
@@ -70,6 +76,14 @@ export default {
 .article-body-content {
   margin-bottom 10px
 }
+.weui-cells__title {
+  margin-top 0.77em
+  margin-bottom 0.3em
+  padding-left 15px
+  padding-right 15px
+  color #999999
+  font-size 14px
+}
 .article-body-files-document {
   list-style none
   .file-item {
@@ -88,14 +102,6 @@ export default {
   }
 }
 .article-body-files-video {
-  .weui-cells__title {
-    margin-top 0.77em
-    margin-bottom 0.3em
-    padding-left 15px
-    padding-right 15px
-    color #999999
-    font-size 14px
-  }
   .article-body-files-video-body {
     padding 15px
     background-color #fff
@@ -109,6 +115,17 @@ export default {
       background-color #000
       border-radius 3px
       overflow hidden
+    }
+  }
+}
+.article-body-files-img {
+  .article-body-files-img-body {
+    padding 10px
+    background-color #fff
+    text-align center
+    img {
+      max-width 90%
+      border solid 2px #f0f0f0
     }
   }
 }
