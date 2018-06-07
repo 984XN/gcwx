7585b01450f8353b117f3c72e8eb8d32d0830b3f
Mr.User <test@test.test>
2018-06-07 17:11:11 +0800

    add 缴纳党费的HTML结构

---
+++ src/view/user/fees.vue
@@ -0,0 +1,9 @@
+<template>
+  // 党费缴纳，此功能未排入计划 20180607
+  <div class="page page-user-fees">
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view>
+  </div>
+</template>
---
+++ src/view/user/feesAdd.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page-user-fees-add">
+    党费缴纳
+  </div>
+</template>
